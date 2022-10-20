e248807e5cb26a6035e82ced6ce32d7df7fd3198
Harikeshav R <r.harikeshav@gmail.com>
2022-10-20 16:29:48 +0530

    Create basic bot structure

---
+++ Turing-Bot/bot/bot.py
@@ -0,0 +1,62 @@
+import logging
+import os
+from fnmatch import fnmatch
+
+import discord
+from discord.ext import commands, tasks
+
+# Setup logger.
+logger = logging.getLogger(__name__)
+
+
+# Create bot.
+class Bot(commands.Bot):
+    def __init__(self):
+        # Get all intents available for the bot.
+        intents = discord.Intents().all()
+
+        # Add debug guilds because registering slash commands take upwards of an hour.
+        super().__init__(
+            debug_guilds=[1032523257796104273, 1012219323898679306], intents=intents
+        )
+
+        # Load all cogs
+        self.load_cogs()
+
+    def load_cogs(self):
+        # Get all available cogs in bot/cogs directory and load them.
+        cog_list = [
+            os.path.splitext(
+                ".".join(os.path.join(os.path.relpath(path), name).split("/"))
+            )[0]
+            for path, sub_dirs, files in os.walk("bot/cogs")
+            for name in files
+            if fnmatch(name, "*.py")
+        ]
+
+        for cog in cog_list:
+            if "__init__" in cog:
+                continue
+
+            logger.info(f"Loading {cog}...")
+
+            try:
+                self.load_extension(cog)
+                logger.info(f"Successfully loaded {cog}!")
+
+            except Exception as e:
+                logger.error(f"Could not load {cog}! {str(e)}")
+
+    async def on_ready(self):
+        await self.wait_until_ready()
+
+        logger.info(f"Logged in as {self.user}!")
+
+        # Change presence
+        await self.change_presence(
+            status=discord.Status.online,
+            activity=discord.Activity(
+                name=f"Participants In Regium Turing",
+                type=discord.ActivityType.watching,
+            ),
+        )
---
+++ Turing-Bot/main.py
@@ -0,0 +1,38 @@
+import logging
+import os
+
+import anvil.server
+from bot.bot import Bot
+from dotenv import load_dotenv
+from rich.logging import RichHandler
+
+load_dotenv()
+
+# Get discord token
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+ANVIL_CLIENT_KEY = os.getenv("ANVIL_CLIENT_KEY")
+
+# Setup logging using rich's logging handler.
+FORMAT = "%(message)s"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=FORMAT,
+    datefmt="[%X]",
+    handlers=[RichHandler(markup=True)],
+)
+
+logger = logging.getLogger()
+
+
+def main(token):
+    # Connect to the anvil leaderboards server.
+    anvil.server.connect(ANVIL_CLIENT_KEY, url="ws://turing.regium22.com/_/uplink")
+
+    # Run the bot.
+    bot = Bot()
+    bot.run(token)
+
+
+if __name__ == "__main__":
+    main(DISCORD_TOKEN)
